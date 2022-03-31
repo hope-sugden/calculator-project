6a22924e1ebbd4d519fc3fddc1f60309ab15e4f1
Hope Sugden <hope_8@hotmail.co.uk>
2022-03-31 18:59:53 +0100

    scss files created and functions added

--- main.js
+++ main.js
@@ -2,6 +2,62 @@
 // when you add a symbol it should show instead of the first number. 
 // the second number will show instead of the symbol.
 // when equals is pressed, the calcualtion is completed and the answer is shown in the output.
+const getCurrentOperator = () => {
+    operators.forEach(operator => {
+        if (display.innerHTML.includes(operator)) {
+          console.log(operator);
+            currentOperator = operator;
+        }
+    }) 
+}
+
+const calculateEquationResult = () => {
+    const displayArray = display.innerHTML.split(currentOperator);
+    console.log(displayArray);
+      if(currentOperator == "+") {
+        return result.innerHTML = parseFloat(displayArray[0]) + parseFloat(displayArray[1]);
+      }
+    
+      else if(currentOperator == "-"){
+          return result.innerHTML = parseFloat(displayArray[0]) - parseFloat(displayArray[1]);
+      }
+    
+      else if(currentOperator == "*") {
+          return result.innerHTML = parseFloat(displayArray[0]) * parseFloat(displayArray[1]);
+      }
+    
+      else if(currentOperator == "/") {
+          return result.innerHTML = parseFloat(displayArray[0]) / parseFloat(displayArray[1]);
+      }
+      
+      else {
+          alert("calculation not recognised!")
+      }
+}
+
+const calculateEquationDisplay = () => {
+    const displayArray = display.innerHTML.split(currentOperator);
+    console.log(displayArray);
+      if(currentOperator == "+") {
+        return display.innerHTML = parseFloat(displayArray[0]) + parseFloat(displayArray[1]);
+      }
+    
+      else if(currentOperator == "-"){
+          return display.innerHTML = parseFloat(displayArray[0]) - parseFloat(displayArray[1]);
+      }
+    
+      else if(currentOperator == "*") {
+          return display.innerHTML = parseFloat(displayArray[0]) * parseFloat(displayArray[1]);
+      }
+    
+      else if(currentOperator == "/") {
+          return display.innerHTML = parseFloat(displayArray[0]) / parseFloat(displayArray[1]);
+      }
+      
+      else {
+          alert("calculation not recognised!")
+      }
+}
 
 const display = document.getElementById("display");
 const button1 = document.getElementById("one");
@@ -89,7 +145,15 @@ const buttonX = document.getElementById("multiply");
 
 buttonX.addEventListener("click", () => {
     console.log(buttonX.innerHTML);
-    display.innerHTML += "*";
+    getCurrentOperator();
+    if(display.innerHTML.includes(operators)) {
+           calculateEquationDisplay();
+           return display.innerHTML += "*"
+    }
+    else {
+        display.innerHTML += "*";
+    }
+
 })
 
 
@@ -97,14 +161,28 @@ const buttonSubtract = document.getElementById("subtract");
 
 buttonSubtract.addEventListener("click", () => {
     console.log(buttonSubtract.innerHTML);
-    display.innerHTML += "-";
+    getCurrentOperator();
+    if(display.innerHTML.includes(operators)) {
+           calculateEquationDisplay();
+           return display.innerHTML += "-"
+    }
+    else {
+        display.innerHTML += "-";
+    }
 })
 
 const buttonPlus = document.getElementById("plus");
 
 buttonPlus.addEventListener("click", () => {
     console.log(buttonPlus.innerHTML);
-    display.innerHTML += "+";
+    getCurrentOperator();
+    if(display.innerHTML.includes(operators)) {
+           calculateEquationDisplay();
+           return display.innerHTML += "+"
+    }
+    else {
+        display.innerHTML += "+";
+    }
 })
 
 
@@ -114,11 +192,8 @@ let currentOperator = ""
 
 buttonPercent.addEventListener("click", () => {
     display.innerHTML += "%";
-    operators.forEach(operator => {
-        if (display.innerHTML.includes(operator)) {
-            currentOperator = operator;
-        }
-    }) 
+    getCurrentOperator();
+    
     const displayArray = display.innerHTML.split(currentOperator);
     console.log(displayArray);
     if(currentOperator == "+") {
@@ -148,7 +223,14 @@ const buttonDivide = document.getElementById("divide");
 
 buttonDivide.addEventListener("click", () => {
     console.log(buttonDivide.innerHTML);
-    display.innerHTML += "/";
+    getCurrentOperator();
+    if(display.innerHTML.includes(operators)) {
+           calculateEquationDisplay();
+           return display.innerHTML += "/"
+    }
+    else {
+        display.innerHTML += "/";
+    }
 })
 
 const buttonDecimal = document.getElementById("dot");
@@ -165,45 +247,31 @@ buttonClear.addEventListener("click", () => {
     result.innerHTML = "";
 })
 
-                            const buttonSign = document.getElementById("sign");
+const buttonSign = document.getElementById("sign");
+buttonSign.addEventListener("click",() => {
+    getCurrentOperator();
+    const displayArray = display.innerHTML.split(currentOperator);
+    parseFloat(displayArray[displayArray.length])
 
-                            buttonSign.addEventListener("click",() => {
-                                const displayArray = display.innerHTML.split(currentOperator);
-                                parseFloat(displayArray[displayArray.length])
-                                let changeSign = displayArray.splice(displayArray.length,1,(displayArray.length * (-1)));
-                                return changeSign = display.innerHTML;
-                            })
+    if(displayArray[1].charAt(0) == "-" ) {
+        const positiveNumber = Math.abs(displayArray[1])
+        console.log(positiveNumber);
+        displayArray.splice(1,1,positiveNumber)
+    }
+
+    else {
+        const negativeNumber =  "-".concat(displayArray[1])
+        displayArray[1] = negativeNumber
+        console.log(displayArray);
+    }
+displayArray.splice(1,0,currentOperator);
+display.innerHTML = displayArray.join("");
+})
 
 const buttonEquals = document.getElementById("equals");
 const result = document.getElementById("result")
 
 buttonEquals.addEventListener("click", (event) => {
-    operators.forEach(operator => {
-        if (display.innerHTML.includes(operator)) {
-          console.log(operator);
-            currentOperator = operator;
-        }
-    }) 
-const displayArray = display.innerHTML.split(currentOperator);
-console.log(displayArray);
-  if(currentOperator == "+") {
-    return result.innerHTML = parseFloat(displayArray[0]) + parseFloat(displayArray[1]);
-  }
-
-  else if(currentOperator == "-"){
-      return result.innerHTML = parseFloat(displayArray[0]) - parseFloat(displayArray[1]);
-  }
-
-  else if(currentOperator == "*") {
-      return result.innerHTML = parseFloat(displayArray[0]) * parseFloat(displayArray[1]);
-  }
-
-  else if(currentOperator == "/") {
-      return result.innerHTML = parseFloat(displayArray[0]) / parseFloat(displayArray[1]);
-  }
-  
-  else {
-      alert("calculation not recognised!")
-  }
-  
+    getCurrentOperator();
+    calculateEquationResult();  
 })
